f2c0e79d1e80a6382041e75ae43190580509c92c
Dian Pan <dian.di.pan@gmail.com>
2015-10-25 16:33:12 -0700

    add index view for fetcher

--- app/controllers/fetchers_controller.rb
+++ app/controllers/fetchers_controller.rb
@@ -2,13 +2,7 @@ require 'rubygems'
 require 'yql'
 
 class FetchersController < ApplicationController
-  def fetch(symbol, startDate, endDate)
-    yql = Yql::Client.new
-    query = Yql::QueryBuilder.new 'yahoo.finance.historicaldata'
-    query.conditions = {:symbol => symbol, :startDate => startDate, :endDate => endDate}
-    yql.query = query
-    yql.format = json
-    response = yql.get
-    response.show.to_s
+  def index
+    @yahoo = fetch('YHOO', '2009-09-11', '2010-09-13')
   end
 end
--- spec/controllers/fetcher_controller_spec.rb
+++ spec/controllers/fetcher_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe FetchersController do
+describe FetcherController do
   context '#fetch' do
     it 'fetches data from yql' do
       expect(fetch('YHOO', '2009-09-11', '2010-09-13')).to be_success
