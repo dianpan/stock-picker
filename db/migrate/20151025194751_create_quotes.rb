efdc2c4095d10d8fa34b3a13efe2dcb3a997d2a3
Dian Pan <dian.di.pan@gmail.com>
2015-10-25 13:20:03 -0700

    add columns to quotes table

--- db/migrate/20151025194751_create_quotes.rb
+++ db/migrate/20151025194751_create_quotes.rb
@@ -1,6 +1,16 @@
 class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
+      t.references :stock, index: true, foreign_key: true, null: false
+      t.date :date, index: true, null: false
+      t.float :open
+      t.float :close
+      t.float :high
+      t.float :low
+      t.integer :volume
+      t.float :adj_close
+
+      t.timestamps null:false
     end
   end
 end
