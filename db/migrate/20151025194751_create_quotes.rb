7e941f91eca175afcbe7e4f301d2473bf4d61e5d
Dian Pan <dian.di.pan@gmail.com>
2015-10-25 13:54:39 -0700

    update col names to be explicit that value contains cents

--- db/migrate/20151025194751_create_quotes.rb
+++ db/migrate/20151025194751_create_quotes.rb
@@ -3,12 +3,12 @@ class CreateQuotes < ActiveRecord::Migration
     create_table :quotes do |t|
       t.references :stock, index: true, foreign_key: true, null: false
       t.date :date, index: true, null: false
-      t.integer :open, :limit => 8
-      t.integer :close, :limit => 8
-      t.integer :high, :limit => 8
-      t.integer :low, :limit => 8
+      t.integer :open_cents, :limit => 8
+      t.integer :close_cents, :limit => 8
+      t.integer :high_cents, :limit => 8
+      t.integer :low_cents, :limit => 8
       t.integer :volume
-      t.integer :adj_close, :limit => 8
+      t.integer :adj_close_cents, :limit => 8
       t.string :currency, default: "USD", null: false
 
       t.timestamps null:false
