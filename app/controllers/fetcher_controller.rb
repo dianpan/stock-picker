2a3ac727ceb517b5df88b44abd21c0cae9f644b6
Dian Pan <dian.di.pan@gmail.com>
2015-10-25 16:11:36 -0700

    add rspec gem

--- app/controllers/fetcher_controller.rb
+++ app/controllers/fetcher_controller.rb
@@ -2,5 +2,13 @@ require 'rubygems'
 require 'yql'
 
 class FetcherController < ApplicationController
-
+  def fetch(symbol, startDate, endDate)
+    yql = Yql::Client.new
+    query = Yql::QueryBuilder.new 'yahoo.finance.historicaldata'
+    query.conditions = {:symbol => symbol, :startDate => startDate, :endDate => endDate}
+    yql.query = query
+    yql.format = json
+    response = yql.get
+    response.show.to_s
+  end
 end
