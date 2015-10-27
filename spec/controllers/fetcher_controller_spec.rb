88ca170f299f7366d94bbc268782c79e9e3f704b
Dian Pan <dian.di.pan@gmail.com>
2015-10-27 09:49:06 -0700

    remove rspec test

--- spec/controllers/fetcher_controller_spec.rb
+++ spec/controllers/fetcher_controller_spec.rb
@@ -1,9 +1,4 @@
 require 'rails_helper'
 
-describe FetcherController do
-  context '#fetch' do
-    it 'fetches data from yql' do
-      expect(fetch('YHOO', '2009-09-11', '2010-09-13')).to be_success
-    end
-  end
+describe FetchersController do
 end
