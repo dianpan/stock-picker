0bb2aa45403b0ba58d685df043f75f62564f62be
Dian Pan <dian.di.pan@gmail.com>
2015-10-27 09:44:40 -0700

    update fetcher model to include yql query

--- app/models/fetcher.rb
+++ app/models/fetcher.rb
@@ -1,2 +1,14 @@
-class Fetcher < ActiveRecord::Base
+require 'json'
+require 'net/http'
+
+def get_yql_data(ticker, startDate, endDate)
+  url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22#{ticker}%22%20and%20startDate%20%3D%20%22#{startDate}%22%20and%20endDate%20%3D%20%22#{endDate}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+  resp = Net::HTTP.get_response(URI.parse(url))
+  result = JSON.parse(resp.body)
+  return result
 end
+
+puts JSON.pretty_generate(get_yql_data("YHOO", "2015-01-01", "2015-08-26"))
+
+
+
