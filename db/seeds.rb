6948e1675a31039c739294937dac701582393930
Dian Pan <dian.di.pan@gmail.com>
2015-10-27 19:08:55 -0700

    update variable names to be more concise

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
 
 require 'csv'
 
-col_data = []
+symbol_data = []
 CSV.foreach(File.dirname(__FILE__) + '/stocklist-all.csv', :headers => true) do |row|
-  col_data << row.to_h
+  symbol_data << row.to_h
 end
 
-col_data.each do |hash|
+symbol_data.each do |hash|
   key, value = hash.first
   new_hash = {key => value}
-  Stock.create!(new_hash)
+  Stock.create(new_hash)
 end
 
