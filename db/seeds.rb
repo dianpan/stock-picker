f66bfd04687647195626dc7b1618b6a9458a6425
Dian Pan <dian.di.pan@gmail.com>
2015-11-01 18:40:10 -0800

    remove unneeded files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,17 @@
-require 'seeds/stock_symbol'
+require 'csv'
+
+symbol_data = []
+CSV.foreach(File.dirname(__FILE__) + '/stocklist-all.csv', :headers => true) do |row|
+  symbol_data << row.to_h
+end
+
+existing_stocks = Stock.all.map { |s| s.symbol }
+
+symbol_data.each do |hash|
+  key, value = hash.first
+  new_hash = {key => value}
+  unless existing_stocks.include?(value)
+    Stock.create(new_hash)
+  end
+end
+
