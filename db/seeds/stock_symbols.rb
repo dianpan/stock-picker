5b3cd8714ca6a9d899204e09baad3b80dced0278
Dian Pan <dian.di.pan@gmail.com>
2015-11-01 18:26:34 -0800

    add idempotency for stock seed file

--- db/seeds/stock_symbols.rb
+++ db/seeds/stock_symbols.rb
@@ -5,9 +5,13 @@ CSV.foreach(File.dirname(__FILE__) + '/stocklist-all.csv', :headers => true) do 
   symbol_data << row.to_h
 end
 
+existing_stocks = Stock.all
+
 symbol_data.each do |hash|
   key, value = hash.first
   new_hash = {key => value}
-  Stock.create(new_hash)
+  unless existing_stocks.include?(new_hash)
+    Stock.create(new_hash)
+  end
 end
 
